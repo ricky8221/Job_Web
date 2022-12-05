e3f87de87c4aa7d90dee292d60cf4becd3bb0d29
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-12-05 13:20:48 -0800

    added create. rename index, show. fixed layout

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class ListingController extends Controller
+{
+    public function index()
+    {
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+        ]);
+    }
+
+    // Show single listing
+    public function show(Listing $listing)
+    {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+
+    // Show Create Form
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    // Store Form
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email', Rule::unique('listings', 'email')],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,19 +16,30 @@ use App\Models\Listing;
 |
 */
 
+/*
+Common Resource Routes:
+index - Show all listings                    ✅
+show - Show all listing                      ✅
+create - Show form to create new listing     ✅
+store - Store new listing                    ✅
+edit - Show form to edit listing
+update - Update listing
+destroy - Delete listing
+*/
+
 // All Listing
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
+
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store New Listing
+Route::POST('/listings/', [ListingController::class, 'store']);
+
+
+
+
+
 
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    if($listing){
-        return view('listing', [
-            'listing' => $listing
-        ]);
-    }
-
-});
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
