4cd67613eed3525628abb23a264fa1e5f8a1f725
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-12-05 10:18:05 -0800

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(5)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel JS',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'website' => 'laravel.com',
+        //     'email' => 'email@gmail.com',
+        //     'description' => 'Lorem'
+        // ]);
+
+        // Listing::create([
+        //     'title' => 'Laravel Junior Developer',
+        //     'tags' => 'laravel JS',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Los Angeles, CA',
+        //     'website' => 'laravel.com',
+        //     'email' => 'ricky@gemail.com',
+        //     'description' => 'Lorem'
+        // ]);
+
+        Listing::factory(6)->create();
+    }
+}
---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,4 @@
+<div {{$attributes->merge(['class'=>'class="bg-gray-50 border boder-gray-200 rounded p-6"'])
+}} >
+{{$slot}}
+</div>
