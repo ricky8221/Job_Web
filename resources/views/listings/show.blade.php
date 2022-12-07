307b3b6e5c2b77b792223989ac1ab36568060f0e
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2022-12-06 19:48:30 -0800

    added only the poster can edit or delete the post

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -48,16 +48,20 @@
     </div>
 </x-card>
 
+
 <x-card class="mt-4 p-2 flex space-x-6">
+    @unless ($listing->user_id != auth()->id())
     <a href="/listings/{{$listing->id}}/edit">
         <i class="fa-solid fa-pencil"></i> Edit
     </a>
-
     <form method="POST" action="/listings/{{$listing->id}}">
         @csrf
         @method('DELETE')
         <button class="text-red-500"><i class="fa-solid fa-trash"></i> Delete</button>
     </form>
+    @else
+
+    @endunless
 </x-card>
 </div>
 </x-layout>
